84a76ace4909e31ca6e4276eae21e226ae37e153
Daniel Chen <team6612@gmail.com>
2016-09-15 01:38:05 +0800

    fix hide fail and ajax error handle

--- app/components/Home.js
+++ app/components/Home.js
@@ -118,43 +118,41 @@ class Home extends React.Component {
                   {columnList}
                 </ul>
               </div>
-              /*
-               *<div className="dropdown">
-               *  <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-               *    篩選器
-               *    <span className="caret"></span>
-               *  </button>
-               *  <div className="dropdown-menu" aria-labelledby="dropdownMenu2" id="filter">
-               *    <li>時間</li>
-               *    <li>
-               *      <div className="btn-group" role="group" aria-label="..." id="day">
-               *        <button type="button" className="btn btn-default">一</button>
-               *        <button type="button" className="btn btn-default">二</button>
-               *        <button type="button" className="btn btn-default">三</button>
-               *        <button type="button" className="btn btn-default">四</button>
-               *        <button type="button" className="btn btn-default">五</button>
-               *      </div>
-               *    </li>
-               *    <li>
-               *      <div className="btn-group" role="group" aria-label="..." id="time">
-               *        <div className="row-1">  
-               *          <button type="button" className="btn btn-default">1</button>
-               *          <button type="button" className="btn btn-default">2</button>
-               *          <button type="button" className="btn btn-default">3</button>
-               *          <button type="button" className="btn btn-default">4</button>
-               *          <button type="button" className="btn btn-default">N</button>
-               *        </div>
-               *        <div className="row-2">  
-               *          <button type="button" className="btn btn-default">5</button>
-               *          <button type="button" className="btn btn-default">6</button>
-               *          <button type="button" className="btn btn-default">7</button>
-               *          <button type="button" className="btn btn-default">8</button>
-               *          <button type="button" className="btn btn-default">9</button>
-               *        </div>
-               *      </div>
-               *    </li>
-               *  </div>
-               */
+              <div className="dropdown" style={{display:'none'}}>
+                <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                  篩選器
+                  <span className="caret"></span>
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenu2" id="filter">
+                  <li>時間</li>
+                  <li>
+                    <div className="btn-group" role="group" aria-label="..." id="day">
+                      <button type="button" className="btn btn-default">一</button>
+                      <button type="button" className="btn btn-default">二</button>
+                      <button type="button" className="btn btn-default">三</button>
+                      <button type="button" className="btn btn-default">四</button>
+                      <button type="button" className="btn btn-default">五</button>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="btn-group" role="group" aria-label="..." id="time">
+                      <div className="row-1">  
+                        <button type="button" className="btn btn-default">1</button>
+                        <button type="button" className="btn btn-default">2</button>
+                        <button type="button" className="btn btn-default">3</button>
+                        <button type="button" className="btn btn-default">4</button>
+                        <button type="button" className="btn btn-default">N</button>
+                      </div>
+                      <div className="row-2">  
+                        <button type="button" className="btn btn-default">5</button>
+                        <button type="button" className="btn btn-default">6</button>
+                        <button type="button" className="btn btn-default">7</button>
+                        <button type="button" className="btn btn-default">8</button>
+                        <button type="button" className="btn btn-default">9</button>
+                      </div>
+                    </div>
+                  </li>
+                </div>
               </div>
             </h3>
             <div className='panel'>
